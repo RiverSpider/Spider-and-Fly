4c020e886bdafd117376a4986bdf597ad195fe94
RiverSpider <raevskih8888@mail.ru>
2023-02-12 20:26:44 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApplication12/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApplication12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            string[] r = Console.ReadLine().Split();
+            a = Convert.ToInt32(r[0]);
+            b = Convert.ToInt32(r[1]);
+            c = Convert.ToInt32(r[2]);
+            int sx, sy, sz;
+            r = Console.ReadLine().Split();
+            sx = Convert.ToInt32(r[0]);
+            sy = Convert.ToInt32(r[1]);
+            sz = Convert.ToInt32(r[2]);
+            int fx, fy, fz;
+            r = Console.ReadLine().Split();
+            fx = Convert.ToInt32(r[0]);
+            fy = Convert.ToInt32(r[1]);
+            fz = Convert.ToInt32(r[2]);
+            int x = 0, y = 0, z = 0;
+            double s = 0;
+            if(sx == fx){
+                y = (sy - fy);
+                z = (sz - fz);
+            }
+            if (sy == fy)
+            {
+                x = (sx - fx);
+                z = (sz - fz);
+            }
+            if (sz == fz)
+            {
+                y = (sy - fy);
+                x = (sx - fx);
+            }
+            if (((sz == c || sz == 0) && (fx == a || fx == 0)) || ((fz == c || fz == 0) && (sx == a || sx == 0)))
+            {
+                if ((sz == c && fx == 0) || (fz == c && sx == a)) {
+                    x = (c - fz) + (sx);
+                    y = sy - fy;
+                }
+                if ((fz == c && sx == 0) || (sz == c && fx == a))
+                {
+                    x = (c - sz) + (fx);
+                    y = fy - sy;
+                }
+            }
+            if (((sz == c || sz == 0) && (fy == a || fy == 0)) || ((fz == c || fz == 0) && (sy == a || sy == 0)))
+            {
+                if ((sz == c && fy == 0) || (fz == c && sy == a))
+                {
+                    y = (c - fz) + (sy);
+                    x = sx - fx;
+                }
+                if ((fz == c && sy == 0) || (sz == c && fy == a))
+                {
+                    y = (c - sz) + (fy);
+                    x = fx - sx;
+                }
+            }
+            if (((sx == a || sx == 0) && (fy == a || fy == 0)) || ((fx == a || fx == 0) && (sy == a || sy == 0)))
+            {
+                if ((sx == a && fy == b) || (fx == 0 && sy == 0))
+                {
+                    y = (c - fz) + (sy);
+                    x = (c - sz) + fx;
+                }
+                if ((fz == c && sy == 0) || (sz == c && fy == a))
+                {
+                    y = (c - sz) + (fy);
+                    x = (c - fz) + sx;
+                }
+            }
+            s = Math.Sqrt(x * x + y * y + z * z);
+            Console.WriteLine(s);
+        }
+    }
+}
